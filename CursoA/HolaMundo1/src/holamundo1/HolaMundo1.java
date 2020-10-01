858c41236ec39356130948135c690ac7f66cce2f
jorgepechg <jorgepechgrajales@gmail.com>
2020-09-30 20:56:44 -0500

    Initial commit

---
+++ CursoA/HolaMundo1/src/holamundo1/HolaMundo1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo1;
+
+/**
+ *
+ * @author jorge
+ */
+public class HolaMundo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        System.out.println("Te amo abuelita");
+        System.out.println("Quiero que estes bien");
+    }
+    
+}
