d12e270a2cbbdcea7cdc0fb2b1dff485e7601ed9
jorgepechg <jorgepechgrajales@gmail.com>
2020-09-30 21:25:13 -0500

    Update HolaMundo1.java

--- CursoA/HolaMundo1/src/holamundo1/HolaMundo1.java
+++ CursoA/HolaMundo1/src/holamundo1/HolaMundo1.java
@@ -16,8 +16,41 @@ public class HolaMundo1 {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
-        System.out.println("Te amo abuelita");
-        System.out.println("Quiero que estes bien");
+        System.out.println("Te amo abuelita");//Te quiero tanto
+        System.out.println("Quiero que estes bien"); 
+        /*
+        Gracias por tanto
+        */
+        
+        //Datos Primitivos
+        
+        short entero1 = 2;
+        
+        byte entero2 = 3;
+        
+        int entero3= 7;
+        
+        float decimal = 3.45f;
+        
+        double decimal2 = 3.675;
+        
+        char caracter = 't';
+        
+        boolean decision = true;
+        
+        //Datos no Primitivos
+        
+        Integer numero0 = null; //los datos no primitivos me permiten almacenar el valor null
+        
+        //En los datos no primitivos puedes utilizar metodos 
+        
+        String palabra = "holi beibi"; //en cadenas utilizamos comillas dobles
+                
+        System.out.println("Numero entero: "+palabra);
+        
+        
+                
+ 
     }
     
 }
