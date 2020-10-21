646fce5d8d5d3388f17340425e792915e3ab42d4
jorgepechg <jorgepechgrajales@gmail.com>
2020-10-20 23:07:14 -0500

    Nuevo proyecto

---
+++ Curso Profesional de Java (CF)/src/main/HolaMundo.java
@@ -0,0 +1,18 @@
+package main;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		//Esto es un comentario
+		
+		/*
+		 * Este es 
+		 * un comentarios
+		 * con saltos de línea
+		 */
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
